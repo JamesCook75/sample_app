2ea58951ed3486c7d5c2d5bf6e943e799fd8be9c
James Cook <tbbucs10@gmail.com>
2017-03-07 09:44:12 -0500

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   test "layout links" do
+    @user = users(:michael)
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
@@ -10,5 +11,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path
+
   end
 end
